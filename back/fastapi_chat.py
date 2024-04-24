f7a62f024e4207b0144c559ea2545128e9f43f3c
jimmy_code <jimmy.fernandez.devia@gmail.com>
2024-04-24 14:45:46 +0200

    deploiement git action test unit

--- back/fastapi_chat.py
+++ back/fastapi_chat.py
@@ -8,6 +8,9 @@ from typing import List
 from openai import AzureOpenAI
 from back.config import CONFIG
 import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 app = FastAPI()
 
--- tests/tests_unit.py
+++ tests/tests_unit.py
@@ -3,6 +3,7 @@ import sys
 import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from back.fastapi_chat import ChatRequest, app
 
 client = TestClient(app)
